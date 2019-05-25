ae23fd9abf243977bb7ce0cc15b005b9cb4e87e4
psliechty <spencerliechty@gmail.com>
2019-05-25 19:32:12 -0400

    lol

---
+++ app/main/public/precache-manifest.5b1323a82666cbc5cad1e1682999c9f2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26be7efe792e99fc437411e23e014daa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e89fecce3cc374020546",
+    "url": "/static/css/main.c13f90a9.chunk.css"
+  },
+  {
+    "revision": "561318b94763c2757798",
+    "url": "/static/js/2.241e526e.chunk.js"
+  },
+  {
+    "revision": "e89fecce3cc374020546",
+    "url": "/static/js/main.d51dcd6e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
