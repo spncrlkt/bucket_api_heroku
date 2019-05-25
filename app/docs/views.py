4fb6c65c0116ed7c2db3254b0105ec6f470dfc21
psliechty <spencerliechty@gmail.com>
2019-05-24 22:52:51 -0400

    Lol

--- app/docs/views.py
+++ app/docs/views.py
@@ -3,7 +3,7 @@ from flask import Blueprint, render_template as view
 docs = Blueprint('docs', __name__, static_folder='static', template_folder='templates')
 
 
-@docs.route('/')
+@docs.route('/docs/')
 def index():
     """
     Show an index template
