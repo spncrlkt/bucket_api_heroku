5f0736d20ede9ed2735b57ed255923431c6d8b5c
psliechty <spencerliechty@gmail.com>
2019-05-27 23:17:00 -0400

    Build Release

---
+++ app/main/public/precache-manifest.b9ff7d7a32c40009574a923c3e2d0bfd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41dc6ef16b214be356a8b50ea96cfc36",
+    "url": "/index.html"
+  },
+  {
+    "revision": "730e5cded9e5c0cf88fb",
+    "url": "/static/css/main.b5258c44.chunk.css"
+  },
+  {
+    "revision": "8416a8f114d9f7d5c0ca",
+    "url": "/static/js/2.d3e3a0f3.chunk.js"
+  },
+  {
+    "revision": "730e5cded9e5c0cf88fb",
+    "url": "/static/js/main.eff2f054.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
